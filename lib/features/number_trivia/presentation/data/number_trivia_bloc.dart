f274d0af8f2b198c0ae70ac3a370135122c8339e
Elizaveta <kashirskayaelizaveta123@gmail.com>
2021-02-12 16:48:02 +0300

    Add BLOC and realizations Event and State

---
+++ lib/features/number_trivia/presentation/data/number_trivia_bloc.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:number_trivia/core/util/input_converted.dart';
+import 'package:number_trivia/features/number_trivia/domain/entities/number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_concrete_number_trivia.dart';
+import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_number_trivia.dart';
+
+part 'number_trivia_event.dart';
+part 'number_trivia_state.dart';
+
+class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
+  final GetConcreteNumberTrivia getConcreteNumberTrivia;
+  final GetRandomNumberTrivia getRandomNumberTrivia;
+  final InputConverter inputConverter;
+
+  NumberTriviaBloc({
+    // Changed the name of the constructor parameter (cannot use 'this.')
+    @required GetConcreteNumberTrivia concrete,
+    @required GetRandomNumberTrivia random,
+    @required this.inputConverter,
+    // Asserts are how you can make sure that a passed in argument is not null.
+    // We omit this elsewhere for the sake of brevity.
+  })  : assert(concrete != null),
+        assert(random != null),
+        assert(inputConverter != null),
+        getConcreteNumberTrivia = concrete,
+        getRandomNumberTrivia = random;
+
+  @override
+  NumberTriviaState get initialState => Empty();
+
+  @override
+  Stream<NumberTriviaState> mapEventToState(
+      NumberTriviaEvent event,
+      ) async* {
+    // TODO: Add Logic
+  }
+}
+
