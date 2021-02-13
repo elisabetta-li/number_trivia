a136ce678b904984ee5f8b47a84c0783c65938c3
Elizaveta <kashirskayaelizaveta123@gmail.com>
2021-02-13 15:20:52 +0300

    Bloc implemented (no complete) + test

--- lib/features/number_trivia/presentation/block/number_trivia_bloc.dart
+++ lib/features/number_trivia/presentation/block/number_trivia_bloc.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
@@ -10,6 +9,11 @@ import 'package:number_trivia/features/number_trivia/domain/usecases/get_random_
 
 part 'number_trivia_event.dart';
 part 'number_trivia_state.dart';
+
+const String SERVER_FAILURE_MESSAGE = 'Server Failure';
+const String CACHE_FAILURE_MESSAGE = 'Cache Failure';
+const String INVALID_INPUT_FAILURE_MESSAGE =
+    'Invalid Input - The number must be a positive integer or zero.';
 
 class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   final GetConcreteNumberTrivia getConcreteNumberTrivia;
@@ -32,11 +36,22 @@ class NumberTriviaBloc extends Bloc<NumberTriviaEvent, NumberTriviaState> {
   @override
   NumberTriviaState get initialState => Empty();
 
+
   @override
   Stream<NumberTriviaState> mapEventToState(
       NumberTriviaEvent event,
       ) async* {
-    // TODO: Add Logic
+    if (event is GetTriviaForConcreteNumber) {
+      final inputEither =
+      inputConverter.stringToUnsignedInteger(event.numberString);
+
+      yield* inputEither.fold(
+            (failure) async* {
+          yield Error(message: INVALID_INPUT_FAILURE_MESSAGE);
+        },
+            (integer) => throw UnimplementedError(),
+      );
+    }
   }
 }
 
